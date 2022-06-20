a209e3630b1ec43877bb81a7e378b4a67af8dfaa
chelmerd <chelmerd@student.42wolfsburg.de>
2022-06-20 14:42:40 +0200

    ex01: add ScavTrap & add new ClapTrap constructor

---
+++ ex01/ClapTrap.cpp
@@ -0,0 +1,161 @@
+#include <string>
+#include <iostream>
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void)
+	: m_hitPoints(10), m_energyPoints(10), m_attackDamage(0) {
+	std::cout << "Default ClapTrap Constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name)
+	: m_name(name), m_hitPoints(10), m_energyPoints(10), m_attackDamage(0) {
+	std::cout << "ClapTrap Constructor called with name: " << name << std::endl;
+}
+
+ClapTrap::ClapTrap(
+	std::string name,
+	unsigned int hitPoints,
+	unsigned int energyPoints,
+	unsigned int attackDamage):
+	m_name(name),
+	m_hitPoints(hitPoints),
+	m_energyPoints(energyPoints),
+	m_attackDamage(attackDamage) {
+	std::cout << "ClapTrap Constructor called with "
+	<< "name: " << name << ", "
+	<< "hit points: " << hitPoints << ", "
+	<< "energy points: " << energyPoints << ", "
+	<< "attack damage: " << attackDamage
+	<< std::endl;
+}
+
+ClapTrap::ClapTrap(ClapTrap const& other):
+	m_name(other.getName()),
+	m_hitPoints(other.getHitPoints()),
+	m_energyPoints(other.getEneryPoints()),
+	m_attackDamage(other.getAttackDamage()) {
+	std::cout << "ClapTrap Copy Constructor called: Creating ClapTrap Copy"
+	<< std::endl;
+}
+
+ClapTrap::~ClapTrap(void) {
+	std::cout << "ClapTrap Destructor called: "
+	<< getName() << " shutdown" << std::endl;
+}
+
+ClapTrap&	ClapTrap::operator=(ClapTrap const& other) {
+	std::cout << "ClapTrap copy assignment operator called" << std::endl;
+	this->m_name = other.getName();
+	this->m_hitPoints = other.getHitPoints();
+	this->m_energyPoints = other.getEneryPoints();
+	this->m_attackDamage = other.getAttackDamage();
+	return *this;
+}
+
+std::string	ClapTrap::getName(void) const { return m_name; }
+
+unsigned int ClapTrap::getHitPoints(void) const { return m_hitPoints; }
+
+unsigned int ClapTrap::getEneryPoints(void) const { return m_energyPoints; }
+
+unsigned int ClapTrap::getAttackDamage(void) const { return m_attackDamage; }
+
+void	ClapTrap::setName(std::string name) { this->m_name = name; }
+
+void	ClapTrap::setHitPoints(unsigned int hitPoints) {
+	this->m_hitPoints = hitPoints;
+}
+
+void	ClapTrap::setEneryPoints(unsigned int energyPoints) {
+	this->m_energyPoints = energyPoints;
+}
+void	ClapTrap::setAttackDamage(unsigned int attackDamage) {
+	this->m_attackDamage = attackDamage;
+}
+
+bool	ClapTrap::isAlive(void) const {
+	return (m_hitPoints > 0) ? true : false;
+}
+
+void	ClapTrap::printStatus(void) const {
+	if (!isAlive()) {
+		std::cerr << "ClapTrap " << m_name << " is dead." << std::endl;
+	} else if (m_energyPoints < 1) {
+		std::cerr << "ClapTrap " << m_name << " has no energy." << std::endl;
+	}
+	std::cout
+	<< "ClapTrap("
+	<< "name=" << m_name << ", "
+	<< "hp=" << m_hitPoints << ", "
+	<< "energy=" << m_energyPoints << ", "
+	<< "dmg=" << m_attackDamage << ")"
+	<< std::endl;
+}
+
+void	ClapTrap::attack(std::string const& target) {
+	if (!this->canAttack(target)) {
+		return ;
+	}
+	std::cout
+	<< "ClapTrap " << this->m_name << " attacks " << target << ", causing "
+	<< this->m_attackDamage << " points of damage!"
+	<< std::endl;
+	this->m_energyPoints--;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount) {
+	if (!this->canTakeDamage(amount)) {
+		return ;
+	}
+	std::cout
+	<< "ClapTrap " << this->m_name << " takes " << amount << " points of damage."
+	<< std::endl;
+	this->m_hitPoints -= amount;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount) {
+	if (!this->canBeRepaired()) {
+		return ;
+	}
+	std::cout
+	<< "ClapTrap " << this->m_name
+	<< " is being repaired and receives" << amount << " health."
+	<< std::endl;
+	this->m_energyPoints--;
+}
+
+bool	ClapTrap::canAttack(std::string const& target) const {
+	if (!isAlive() || m_energyPoints < 1) {
+		std::cerr << this->m_name << " can't attack " << target << ": ";
+		printStatus();
+		return false;
+	}
+	if (target.empty()) {
+		std::cerr << m_name << " can't attack: No target" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool	ClapTrap::canTakeDamage(unsigned int& amount) const {
+	if (!isAlive()) {
+		std::cerr << m_name << " can't take damage: ";
+		printStatus();
+		return false;
+	}
+	if (amount > this->m_hitPoints) {
+		unsigned int const	diff = amount - m_hitPoints;
+		std::cout << "OVERKILL by " << diff << " points. ";
+		amount -= diff;
+	}
+	return true;
+}
+
+bool	ClapTrap::canBeRepaired(void) const {
+	if (!isAlive() || m_energyPoints < 1) {
+		std::cerr << m_name << " can't be repaired: ";
+		printStatus();
+		return false;
+	}
+	return true;
+}
---
+++ ex01/ScavTrap.cpp
@@ -0,0 +1,77 @@
+#include <string>
+#include <iostream>
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(void): ClapTrap(), m_inGateKeeperMode(false) {
+	std::cout << "Default ScavTrap Constructor called" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name)
+	: ClapTrap(name, 100, 50, 20), m_inGateKeeperMode(false) {
+	std::cout << "ScavTrap Constructor called with name: " << name << std::endl;
+}
+
+ScavTrap::ScavTrap(ScavTrap const& other)
+	: ClapTrap(other), m_inGateKeeperMode(other.isInGateKeeperMode()) {
+	std::cout << "ScavTrap Copy Constructor called: Creating ScavTrap Copy"
+	<< std::endl;
+}
+
+ScavTrap::~ScavTrap(void) {
+	std::cout << "ScavTrap Destructor called: "
+	<< getName() << " shutdown" << std::endl;
+}
+
+ScavTrap&	ScavTrap::operator=(ScavTrap const& other) {
+	std::cout << "ScavTrap copy assignment operator called" << std::endl;
+	ClapTrap::operator=(other);
+	this->m_inGateKeeperMode = other.isInGateKeeperMode();
+	return *this;
+}
+
+ScavTrap&	ScavTrap::operator=(ClapTrap const& other) {
+	std::cout << "ScavTrap copy assignment operator called with a ClapTrap" << std::endl;
+	ClapTrap::operator=(other);
+	this->m_inGateKeeperMode = false;
+	return *this;
+}
+
+bool	ScavTrap::isInGateKeeperMode(void) const {
+	return m_inGateKeeperMode;
+}
+
+void	ScavTrap::printStatus(void) const {
+	ClapTrap::printStatus();
+	std::cout
+	<< "ScavTrap("
+	<< "inGateKeeperMode=" << m_inGateKeeperMode << ")"
+	<< std::endl;
+}
+
+void	ScavTrap::attack(std::string const& target) {
+	if (!this->canAttack(target)) {
+		return ;
+	}
+	if (this->m_inGateKeeperMode) {
+		std::cout << "Guarding gates is fun. ";
+	}
+	std::cout
+	<< "ScavTrap " << this->getName() << " attacks " << target << ", causing "
+	<< this->getAttackDamage() << " points of damage!"
+	<< std::endl;
+	this->setEneryPoints(this->getEneryPoints() - 1);
+}
+
+void	ScavTrap::guardGate(void) {
+	if (this->m_inGateKeeperMode) {
+		std::cout
+		<< "ScavTrap " << this->getName() << " already guards the gate"
+		<< std::endl;
+		return ;
+	}
+	std::cout
+	<< "ScavTrap " << this->getName() << " now guards the gate"
+	<< std::endl;
+	this->m_inGateKeeperMode = true;
+}
